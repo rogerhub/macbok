e9160940fbf1563e5bc7f446aa4e937f075f4334
Roger Chen <roger@rogerhub.com>
2016-02-25 22:56:31 -0800

    Add wireshark

--- default.py
+++ default.py
@@ -148,6 +148,7 @@ def main():
     yield Homebrew(cask_package="android-studio")
     yield Homebrew(cask_package="atom")
     yield Homebrew(cask_package="puppet")
+    yield Homebrew(cask_package="wireshark")
 
     assert sys.executable == "/usr/local/opt/python/bin/python2.7", \
         "Please restart your shell and run this script again, so we install using homebrew's python"
